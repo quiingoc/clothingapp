641f6f7c1c443bf86f873d9841bbfb086636c61e
QUI NGOC <nguyenquingoc03@gmail.com>
2023-09-27 21:11:51 +0700

    commit

---
+++ app/src/main/java/edu/huflit/shoppingonl/Customer/LoginActivity.java
@@ -0,0 +1,16 @@
+package edu.huflit.shoppingonl.Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.huflit.shoppingonl.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
